63c1d0bf1371f5f62897e6491fb60548e40f9ae2
Alexandre <alexandre.chevallier4@gmail.com>
2016-05-03 14:43:24 +0200

    api ping updated

--- src/AppBundle/Controller/PingController.php
+++ src/AppBundle/Controller/PingController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+
 class PingController
 {
     /**
@@ -13,6 +14,7 @@ class PingController
     public function pingAction()
     {
         $reponse = new JsonResponse();
+        $reponse->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
         $reponse -> setData(array(
                 'date' => date(("d-m-Y G:i:s"))
             ));
