4ef51cc28e76f6a601d7980bda29601ce48c0806
svandycke <svandycke@gmail.com>
2016-05-10 12:54:40 +0200

    Ajout de la classe Uuer

--- src/ApiBundle/Controller/UserController.php
+++ src/ApiBundle/Controller/UserController.php
@@ -4,24 +4,60 @@ namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use ApiBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
     public function indexAction()
     {
-        $reponse = new JsonResponse();
-        $reponse -> setData(array(
-                'date' => date(("d-m-Y G:i:s"))
-            ));
-        return $reponse;
+        $repository = $this->getDoctrine()->getRepository('ApiBundle:User');
+
+        $users = $repository->findAll();
+
+        $response = array();
+    	foreach ($users as $user) {
+        	$response[] = array(
+        		'id' => $user->getId(),
+            	'name' => $user->getName(),
+            	'firstName' => $user->getFirstName(),
+        	);
+    	}
+
+        return new JsonResponse($response);
     }
 
     public function addAction()
     {
-        $reponse = new JsonResponse();
-        $reponse -> setData(array(
-                'date' => date(("-m-Y G:i:s"))
-            ));
-        return $reponse;
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $a = new User();
+        $a->setFirstName("Marie");
+        $a->setName("GEFFLOT");
+
+        $em->persist($a);
+        $em->flush();
+
+        return new Response("Utilisateur ajouté");
+
+    }
+
+    public function postAction(Request $request){
+
+        $name = $request->request->get('name');
+        $firstName = $request->request->get('firstName');
+        $header = $request->headers->get('APIkey');
+
+        $valeur = $firstName." ".$name." ".$header;
+
+        return new Response($valeur);
+    }
+
+    public function getAction($name){
+
+
+
+        return new Response($name);
     }
 }
---
+++ src/ApiBundle/Entity/User.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="ApiBundle\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=100)
+     */
+    private $firstName;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return User
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string 
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+}
