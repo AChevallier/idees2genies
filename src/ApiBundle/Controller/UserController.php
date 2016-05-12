3fd16337ce14ec96266a48c54d299afe913b14ec
svandycke <svandycke@gmail.com>
2016-05-12 18:19:01 +0200

    Ajout de la méthode Login dans USER

--- src/ApiBundle/Controller/UserController.php
+++ src/ApiBundle/Controller/UserController.php
@@ -8,11 +8,13 @@ use ApiBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class UserController extends Controller
 {
     public function indexAction()
     {
         $repository = $this->getDoctrine()->getRepository('ApiBundle:User');
+        //$users = $repository->findBy(array(), array('name' => 'ASC'),10);
         $users = $repository->findAll();
         $tableUsers = array();
 
@@ -45,7 +47,7 @@ class UserController extends Controller
     }
 
     public function postAction(Request $request){
-        
+
         $params = array();
         $content = $this->get("request")->getContent();
         if (!empty($content)) {
@@ -63,10 +65,49 @@ class UserController extends Controller
         return $object;
     }
 
-    public function getAction($name){
+    // Fonction qui authentifie l'utilisateur
+    public function loginAction(){
 
+        $params = array();
+        $content = $this->get("request")->getContent();
+        if (!empty($content)) {
+            $params = json_decode($content, true);
+        }
 
+        if(!empty($params['login']) && !empty($params['password'])){
 
-        return new Response($name);
+            $em = $this->getDoctrine()->getEntityManager();
+            $repository = $em->getRepository('ApiBundle:User');
+            $user = $repository->findOneBy(array('email' => $params['login']));
+
+            if(password_verify($params['password'], $user->getPassword())){
+
+                try{
+                    $token = bin2hex(openssl_random_pseudo_bytes(20));
+                    $date = new \DateTime();
+                    $date->modify('+1 day');
+
+                    $valideToken = $date->format('Y-m-d H:i:s');
+
+                    $user->setToken($token);
+                    $user->setValideToken($date);
+                    $em->flush();
+
+                    $arr = array('id' => $user->getId(), 'name' => $user->getName(), 'firstName' => $user->getFirstName(), 'email' => $user->getEmail(),  'token' => $token, 'valideToken' => $valideToken);
+                    $reponse = json_encode($arr,JSON_UNESCAPED_UNICODE);
+
+                }
+                catch(Exception $ex){
+                    throw new Exception("Une erreur interne s'est produite.");
+                }
+            }
+            else{
+                throw new Exception("Le login ou le mot de passe est incorrect.");
+            }
+        }else{
+            throw new Exception("Veuillez envoyer le login et le mot de passe.");
+        }
+
+        return new JsonResponse($reponse);
     }
 }
--- src/ApiBundle/Entity/User.php
+++ src/ApiBundle/Entity/User.php
@@ -3,6 +3,7 @@
 namespace ApiBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * User
@@ -35,6 +36,33 @@ class User
      */
     private $firstName;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=100)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="token", type="string", length=250)
+     */
+    private $token;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="valideToken", type="datetime")
+     */
+    private $valideToken;
 
     /**
      * Get id
@@ -60,16 +88,6 @@ class User
     }
 
     /**
-     * Get name
-     *
-     * @return string 
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
      * Set firstName
      *
      * @param string $firstName
@@ -85,10 +103,113 @@ class User
     /**
      * Get firstName
      *
-     * @return string 
+     * @return string
      */
     public function getFirstName()
     {
         return $this->firstName;
     }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     * @return User
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+
+        return $this;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set ValideToken
+     *
+     * @param DateTime $email
+     * @return User
+     */
+    public function setValideToken($valideToken)
+    {
+        $this->valideToken = $valideToken;
+
+        return $this;
+    }
+
+    /**
+     * Get valideToken
+     *
+     * @return DateTime
+     */
+    public function getValideToken()
+    {
+        return $this->valideToken;
+    }
+
 }
