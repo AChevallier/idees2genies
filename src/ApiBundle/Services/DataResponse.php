e905bb184f12b82b6e31c8a3109dde90b31df6e4
svandycke <svandycke@gmail.com>
2016-05-18 18:38:34 +0200

    MAJ response

--- src/ApiBundle/Services/DataResponse.php
+++ src/ApiBundle/Services/DataResponse.php
@@ -16,7 +16,10 @@ class DataResponse
 
         $response =  new Response(json_encode($data,JSON_UNESCAPED_UNICODE), 200);
         $response->headers->set('Access-Control-Allow-Origin', 'http://localhost:3000');
+        $response->headers->set('Content-Type', 'application/json');
 
         return $response;
+
+
     }
 }
