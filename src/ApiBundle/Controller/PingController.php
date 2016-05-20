b85871332f3156b2032b955d8c82b3ec5576056d
svandycke <svandycke@gmail.com>
2016-05-20 19:10:40 +0200

    Ajoute inner ping

--- src/ApiBundle/Controller/PingController.php
+++ src/ApiBundle/Controller/PingController.php
@@ -4,6 +4,13 @@ namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use ApiBundle\Entity\User;
+use ApiBundle\Entity\UserCommunity;
+use ApiBundle\Entity\Community;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+
 
 class PingController extends Controller
 {
@@ -35,5 +42,27 @@ class PingController extends Controller
         );
 
         return $this->get('service_data_response')->JsonResponse($tableData);
+    }
+
+    public  function innerAction(){
+
+        /*$em = $this->getDoctrine()->getEntityManager();
+        $videos = $em->createQuery('select uc, u
+                            from ApiBundle:UserCommunity uc
+                            left join ApiBundle:User u
+                            where uc.idCommunity = u.id')
+            ->getResult();
+
+        $parameters=$videos->getParameters();*/
+
+        // OK ci-après
+
+        /*$em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('SELECT u FROM ApiBundle:User u WHERE u.name = :name')->setParameter('name', 'VANDYCKE');
+
+        $products = $query->getResult();
+        $name = $products[0]->getFirstName();*/
+
+        return new Response("essai");
     }
 }
