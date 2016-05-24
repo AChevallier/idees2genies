29969c285018b06bbdf4b9f84ee29a0fb5e22d62
svandycke <svandycke@gmail.com>
2016-05-24 13:09:07 +0200

    MAJ

--- SQL/symfonyIdees.sql
+++ SQL/symfonyIdees.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Lun 23 Mai 2016 à 22:01
+-- Généré le :  Mar 24 Mai 2016 à 13:07
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -67,20 +67,21 @@ CREATE TABLE `idea` (
   `idCommunauty` int(11) DEFAULT NULL,
   `idUser` int(11) NOT NULL,
   `publicateDate` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `idea`
 --
 
 INSERT INTO `idea` (`id`, `title`, `idea`, `idCommunauty`, `idUser`, `publicateDate`) VALUES
-(1, 'Essai', 'Je ne sais pas', NULL, 2, '2016-05-23 06:31:09'),
+(1, 'Une guitare qui joue de la musique', 'Je ne sais pas', NULL, 2, '2016-05-23 06:31:09'),
 (3, 'Hello', 'Bonjour', 1, 2, '2016-05-23 07:06:05'),
-(6, 'France', 'Français', NULL, 2, '2016-05-24 01:18:29'),
+(6, 'Un téléphone qui passe des appels', 'Français', NULL, 2, '2016-05-24 01:18:29'),
 (7, 'GEII', 'Bonjour', NULL, 2, '2016-05-23 10:02:12'),
 (12, 'APP', 'sdsds', NULL, 2, '2016-05-23 10:43:20'),
 (13, 'Frameword', 'Essa', NULL, 2, '2016-05-23 10:49:35'),
-(16, 'Marie GEFFLOT', 'qdsqsdqdsq', NULL, 2, '2016-05-24 01:00:00');
+(16, 'Un aspirateur qui aspire', 'qdsqsdqdsq', NULL, 2, '2016-05-24 01:00:00'),
+(23, 'Idée d''Hamada', 'Hello comment ça va ?', NULL, 2, '2016-05-24 12:51:29');
 
 -- --------------------------------------------------------
 
@@ -104,8 +105,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `firstName`, `email`, `password`, `token`, `valideToken`, `administrator`) VALUES
-(2, 'VANDYCKE', 'Steve', 'svandycke@gmail.com', 'c3284d0f94606de1fd2af172aba15bf3', '5aa3dc16118761bbb58a96e29cbfe36f9966e328', '2016-05-24 20:03:34', 1),
-(62, 'CHEVALLIER', 'Alexandre', 'alexandre.chevallier4@gmail.com', '8f8c1ff4ce76e757e2000ed4e6926af4', '53ab18ac3389a8a79a9bbbbe8d5511e50b15b0bc', '2016-05-24 09:00:38', 1);
+(2, 'VANDYCKE', 'Steve', 'svandycke@gmail.com', 'c3284d0f94606de1fd2af172aba15bf3', '657ca3afb568d213cc68e5acf28e076f395ee939', '2016-05-25 12:58:49', 1),
+(62, 'CHEVALLIER', 'Alexandre', 'alexandre.chevallier4@gmail.com', '8f8c1ff4ce76e757e2000ed4e6926af4', '062f0c44703b4ed45f198f51d29e730fc94527e7', '2016-05-25 12:59:05', 1);
 
 -- --------------------------------------------------------
 
@@ -117,7 +118,7 @@ CREATE TABLE `user_community` (
   `id` int(11) NOT NULL,
   `idCommunity` int(11) NOT NULL,
   `idUser` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `user_community`
@@ -125,7 +126,8 @@ CREATE TABLE `user_community` (
 
 INSERT INTO `user_community` (`id`, `idCommunity`, `idUser`) VALUES
 (6, 7, 2),
-(7, 1, 62);
+(7, 1, 62),
+(8, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -137,18 +139,18 @@ CREATE TABLE `vote_user_idea` (
   `id` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   `idIdea` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `vote_user_idea`
 --
 
 INSERT INTO `vote_user_idea` (`id`, `idUser`, `idIdea`) VALUES
-(3, 2, 6),
-(4, 2, 1),
-(5, 2, 6),
-(6, 2, 16),
-(7, 2, 1);
+(21, 2, 6),
+(24, 2, 16),
+(25, 2, 13),
+(26, 2, 23),
+(30, 62, 1);
 
 --
 -- Index pour les tables exportées
@@ -208,7 +210,7 @@ ALTER TABLE `community`
 -- AUTO_INCREMENT pour la table `idea`
 --
 ALTER TABLE `idea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
@@ -218,12 +220,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user_community`
 --
 ALTER TABLE `user_community`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `vote_user_idea`
 --
 ALTER TABLE `vote_user_idea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
