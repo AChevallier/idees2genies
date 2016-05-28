49307160825cdf0a02ce8904008d3f5e8b11deac
svandycke <svandycke@gmail.com>
2016-05-28 14:16:22 +0200

    MAJ

--- SQL/symfonyIdees.sql
+++ SQL/symfonyIdees.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:8889
--- Généré le :  Mar 24 Mai 2016 à 13:07
+-- Généré le :  Sam 28 Mai 2016 à 14:15
 -- Version du serveur :  5.5.42
 -- Version de PHP :  5.6.10
 
@@ -43,7 +43,7 @@ CREATE TABLE `community` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `community`
@@ -51,8 +51,11 @@ CREATE TABLE `community` (
 
 INSERT INTO `community` (`id`, `name`, `description`) VALUES
 (1, 'Licence PRISM', 'Le groupe concerne la classe.'),
-(7, 'VANDYCKE', 'Le groupe de la famille'),
-(8, 'DUT GEII', 'Un forum d''entre aide.');
+(7, 'Paris Aéroport', 'Le groupe de la famille'),
+(8, 'DUT GEII', 'Un forum d''entre aide.'),
+(9, 'Licence SRSI', 'Un groupe de partage.'),
+(10, 'Apple', 'Un groupe de partage.'),
+(11, 'Bouygues Telecom', 'Un groupe de partage.');
 
 -- --------------------------------------------------------
 
@@ -67,21 +70,20 @@ CREATE TABLE `idea` (
   `idCommunauty` int(11) DEFAULT NULL,
   `idUser` int(11) NOT NULL,
   `publicateDate` datetime NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `idea`
 --
 
 INSERT INTO `idea` (`id`, `title`, `idea`, `idCommunauty`, `idUser`, `publicateDate`) VALUES
-(1, 'Une guitare qui joue de la musique', 'Je ne sais pas', NULL, 2, '2016-05-23 06:31:09'),
-(3, 'Hello', 'Bonjour', 1, 2, '2016-05-23 07:06:05'),
-(6, 'Un téléphone qui passe des appels', 'Français', NULL, 2, '2016-05-24 01:18:29'),
-(7, 'GEII', 'Bonjour', NULL, 2, '2016-05-23 10:02:12'),
-(12, 'APP', 'sdsds', NULL, 2, '2016-05-23 10:43:20'),
-(13, 'Frameword', 'Essa', NULL, 2, '2016-05-23 10:49:35'),
-(16, 'Un aspirateur qui aspire', 'qdsqsdqdsq', NULL, 2, '2016-05-24 01:00:00'),
-(23, 'Idée d''Hamada', 'Hello comment ça va ?', NULL, 2, '2016-05-24 12:51:29');
+(1, 'Une guitare qui joue de la musique', 'Quo cognito Constantius ultra mortalem modum exarsit ac nequo casu idem Gallus de futuris incertus agitare quaedam conducentia saluti suae per itinera conaretur, remoti sunt omnes de industria milites agentes in civitatibus perviis.', NULL, 2, '2016-05-23 06:31:09'),
+(3, 'Un cuisinier qui cuisine', 'Nam sole orto magnitudine angusti gurgitis sed profundi a transitu arcebantur et dum piscatorios quaerunt lenunculos vel innare temere contextis cratibus parant, effusae legiones, quae hiemabant tunc apud Siden, isdem impetu occurrere veloci. et signis pr', 1, 2, '2016-05-23 07:06:05'),
+(6, 'Un téléphone qui passe des appels', 'Illud tamen clausos vehementer angebat quod captis navigiis, quae frumenta vehebant per flumen, Isauri quidem alimentorum copiis adfluebant, ipsi vero solitarum rerum cibos iam consumendo inediae propinquantis aerumnas exitialis horrebant.', NULL, 2, '2016-05-24 01:18:29'),
+(7, 'Un ordinateur plus puissant que l''humain', 'Quare hoc quidem praeceptum, cuiuscumque est, ad tollendam amicitiam valet; illud potius praecipiendum fuit, ut eam diligentiam adhiberemus in amicitiis comparandis, ut ne quando amare inciperemus eum, quem aliquando odisse possemus. Quin etiam si minus f', 7, 2, '2016-05-27 14:20:33'),
+(8, 'Un logiciel de caisse', 'Sed quid est quod in hac causa maxime homines admirentur et reprehendant meum consilium, cum ego idem antea multa decreverim, que magis ad hominis dignitatem quam ad rei publicae necessitatem pertinerent? Supplicationem quindecim dierum decrevi sententia ', 7, 2, '2016-05-27 14:31:44'),
+(9, 'Une clé électronique', 'Quanta autem vis amicitiae sit, ex hoc intellegi maxime potest, quod ex infinita societate generis humani, quam conciliavit ipsa natura, ita contracta res est et adducta in angustum ut omnis caritas aut inter duos aut inter paucos iungeretur.', 7, 2, '2016-05-27 14:32:59'),
+(10, 'Un chargeur qui permet de charger', 'Homines enim eruditos et sobrios ut infaustos et inutiles vitant, eo quoque accedente quod et nomenclatores adsueti haec et talia venditare, mercede accepta lucris quosdam et prandiis inserunt subditicios ignobiles et obscuros.', 1, 62, '2016-05-27 14:42:51');
 
 -- --------------------------------------------------------
 
@@ -105,8 +107,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `name`, `firstName`, `email`, `password`, `token`, `valideToken`, `administrator`) VALUES
-(2, 'VANDYCKE', 'Steve', 'svandycke@gmail.com', 'c3284d0f94606de1fd2af172aba15bf3', '657ca3afb568d213cc68e5acf28e076f395ee939', '2016-05-25 12:58:49', 1),
-(62, 'CHEVALLIER', 'Alexandre', 'alexandre.chevallier4@gmail.com', '8f8c1ff4ce76e757e2000ed4e6926af4', '062f0c44703b4ed45f198f51d29e730fc94527e7', '2016-05-25 12:59:05', 1);
+(2, 'Vandycke', 'Steve', 'svandycke@gmail.com', 'c3284d0f94606de1fd2af172aba15bf3', '63515075dc24006ed00fb7ae98a271c0807644dd', '2016-05-29 11:35:04', 1),
+(62, 'Chevallier', 'Alexandre', 'alexandre.chevallier4@gmail.com', '8f8c1ff4ce76e757e2000ed4e6926af4', '8a256e92114be8c7c2ecf92c47d21e3f401456b2', '2016-05-28 14:33:43', 1);
 
 -- --------------------------------------------------------
 
@@ -118,7 +120,7 @@ CREATE TABLE `user_community` (
   `id` int(11) NOT NULL,
   `idCommunity` int(11) NOT NULL,
   `idUser` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `user_community`
@@ -127,7 +129,10 @@ CREATE TABLE `user_community` (
 INSERT INTO `user_community` (`id`, `idCommunity`, `idUser`) VALUES
 (6, 7, 2),
 (7, 1, 62),
-(8, 1, 2);
+(8, 1, 2),
+(9, 10, 62),
+(10, 9, 62),
+(11, 11, 2);
 
 -- --------------------------------------------------------
 
@@ -139,18 +144,22 @@ CREATE TABLE `vote_user_idea` (
   `id` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
   `idIdea` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=105 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `vote_user_idea`
 --
 
 INSERT INTO `vote_user_idea` (`id`, `idUser`, `idIdea`) VALUES
-(21, 2, 6),
-(24, 2, 16),
-(25, 2, 13),
-(26, 2, 23),
-(30, 62, 1);
+(85, 62, 3),
+(93, 2, 7),
+(95, 2, 9),
+(96, 62, 8),
+(97, 62, 7),
+(98, 62, 1),
+(99, 62, 10),
+(101, 2, 10),
+(104, 2, 8);
 
 --
 -- Index pour les tables exportées
@@ -205,12 +214,12 @@ ALTER TABLE `commentary_idea`
 -- AUTO_INCREMENT pour la table `community`
 --
 ALTER TABLE `community`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT pour la table `idea`
 --
 ALTER TABLE `idea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
@@ -220,12 +229,12 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user_community`
 --
 ALTER TABLE `user_community`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT pour la table `vote_user_idea`
 --
 ALTER TABLE `vote_user_idea`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=105;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
