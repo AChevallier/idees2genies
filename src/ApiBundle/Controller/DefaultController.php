83587804d8a6c23d2fb07dbf707768dd6b855607
svandycke <svandycke@gmail.com>
2016-06-04 20:23:08 +0200

    Ajout du pdf

--- src/ApiBundle/Controller/DefaultController.php
+++ src/ApiBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('ApiBundle:Default:index.html.twig');
+
     }
 }
